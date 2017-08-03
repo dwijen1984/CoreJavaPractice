3feb9f0103694c2fad9c3ac2408fd26f18460d25
dkirtan <dwijen.kirtania@searshc.com>
2017-08-02 18:43:21 -0700

    First Commit and push

---
+++ CoreJavaPractise/src/main/java/FirstTestingClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by dkirtan on 8/2/17.
+ */
+public class FirstTestingClass {
+    public static void main(String args[]){
+        System.out.println("This is the first committed file in GitHub");
+    }
+}
